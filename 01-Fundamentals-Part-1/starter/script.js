ea8a32156687c6174d4ed643b8ee41c318650b14
Andreas Zweili <andreas@2li.ch>
2020-12-14 15:31:08 +0100

    13. let, const and var

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,4 +1,18 @@
+// let
+/*
 let js = 'amazing';
 
 let firstName = "Jonas";
 console.log(firstName)
+*/
+
+// var and const
+
+const birthday = "01.01.1970";
+
+// this is not allowed
+// const birthday;
+
+// old
+var foo = 'bar';
+foo = 'baz';

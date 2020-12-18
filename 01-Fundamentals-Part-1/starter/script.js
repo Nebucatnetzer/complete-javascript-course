6c9a9e5b6caced3b55057c66cb6af89b83a407bb
Andreas Zweili <zweili@contria.com>
2020-12-18 16:31:23 +0100

    comment out the if / else statement

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -41,7 +41,7 @@ console.log(markHigherBmi2)
 */
 
 // if / else
-
+/*
 const age = 15;
 
 if (age >= 18) {
@@ -50,3 +50,14 @@ if (age >= 18) {
     const yearsLeft = 18 - age;
     console.log(`Sarah is too young wait another ${yearsLeft} years.`);
 }
+
+const birthYear = 1991;
+
+let century;
+if (birthYear <= 2000) {
+    century = 20;
+} else {
+    century = 21;
+}
+console.log(century);
+*/

7a16150d696bf54e9c8bcf9e5dbb304015a32df5
Andreas Zweili <andreas@2li.ch>
2020-12-22 22:34:58 +0100

    add example for arrays

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -118,3 +118,19 @@ const secondRound = checkWinner(
     calcAverage(85, 54, 41),
     calcAverage(23, 34, 27));
 */
+
+// arrays
+
+// literal syntax
+/*
+const friends = ['Michael', 'Steven', 'Peter'];
+console.log(friends);
+
+const years = new Array(1991, 1984, 2008, 2020);
+
+console.log(friends[0]);
+console.log(friends.length);
+console.log(friends[friends.length - 1]);
+*/
+// not possible because friends is a const
+// friends = ['Anna', 'Maya'];

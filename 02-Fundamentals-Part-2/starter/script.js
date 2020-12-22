c387e501dd02eb896d4acc9ddee9422486af3455
Andreas Zweili <andreas@2li.ch>
2020-12-22 16:28:56 +0100

    add example for strict mode

---
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,13 @@
+'use strict';
+
+// Strict mode example
+/*
+let hasDriversLicense = false;
+const passTest = true;
+
+// This code won't run because it uses an undefined variable
+if (passTest) hasDriverLicense = true;
+if (hasDriversLicense) {
+    console.log('I can drive :)');
+}
+*/

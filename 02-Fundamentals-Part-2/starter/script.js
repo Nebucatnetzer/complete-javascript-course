4e5e82f92a75361516dd9506e2685606577ce423
Andreas Zweili <andreas@2li.ch>
2020-12-22 21:11:46 +0100

    add a multi line arrow function

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -47,4 +47,12 @@ const calcAge2 = function (birthYear) {
 // arrow function
 /*
 const calcAge3 = birthYear => 2037 - birthYear;
+
+const yearsUntilRetirement = (birthYear, firstName) => {
+    const age = 2037 - birthYear;
+    const retirement = 65 - age;
+    return `${firstName} retires in ${retirement} years`;
+}
+
+console.log(yearsUntilRetirement(1991, 'Max'));
 */

d9ff619c1e8251af5b0cab3df1389ff1effae8cb
Andreas Zweili <andreas@2li.ch>
2020-12-22 17:27:40 +0100

    add a function example

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -11,3 +11,20 @@ if (hasDriversLicense) {
     console.log('I can drive :)');
 }
 */
+
+// functions
+/*
+function logger() {
+    console.log('Some default text');
+}
+
+logger();
+
+
+function fruitProcessor(apples, oranges) {
+    const juice = `Juice with ${apples} apples and ${oranges} oranges`;
+    return juice;
+}
+
+console.log(fruitProcessor(3, 4));
+*/
